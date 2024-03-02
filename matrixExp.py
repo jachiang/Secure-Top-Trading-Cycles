dfc9f7d87bb4a346bd0a150b8de44793956c00a6
James Chiang <james.chiangwu@gmail.com>
2024-03-02 12:59:00 +0100

    Minor.

--- matrixExp.py
+++ matrixExp.py
@@ -1,21 +1,5 @@
 from functools import reduce
 
-# Define matrix
-m = [[1,2,3],
-     [4,5,6],
-     [7,8,9]]
-
-# Memoization table
-# M_{r,i} x M_{i,j} x M_{j,c}
-
-# Determine sizes of elements
-# 2^2 + 2^1 + 2^0
-
-# Key for tables: 
-# List of indices (i,j),(j,k),(k,l),(l,m) -> M_{i,j} M_{j,k} M_{k,l} M_{l,m} 
-# To produce key, must sort list of indices (order doesn't matter).
-# key <-- hash(tuple(sorted(indexList)))
-
 class MultLookup:
     def __init__(self, matrix):
         self.table = {}
@@ -31,54 +15,59 @@ class MultLookup:
         key = hash(tuple(sorted(indexPairList)))
         value = self.table.get(key)
         if value:
-            print("hit")
             return value 
         else:
-            elemContainer = []
-            # If number of elements is a power of two.
+            # If there is no hit, populate container with elements for multiplication:
+            # Container elements are multiplicative terms of "powers-of-two" elements 
+            # indicated in indexPairList. The multiplication of these terms in the container
+            # is performed at the end of this branch.
+            elemContainer = [] 
+            # Case 0: If number of matrix elements referenced in indexPairList is a power of two.
+            # e.g M_i,j M_j,k M_k,l M_l,m (4 matrix elements = 2^2)
             if sum(binList) == 1:
-                # If number of elements is 2.
+                # Case 0a: If number of matrix elements is 2.
                 if len(indexPairList) == 2:
                     # Retrieve elements in matrix
                     leftElement = self.matrix[indexPairList[0][0]][indexPairList[0][1]]
                     rightElement = self.matrix[indexPairList[1][0]][indexPairList[1][1]]
-                    res = leftElement * rightElement
-                    newKey = hash(tuple(sorted(indexPairList)))
-                    self.table[newKey] = res
-                    return res
-                # If number of elements is power of 2.
+                # Case 0b: If number of matrix elements is (greater than 1) power of 2.
                 else:
-                    # recursive calls.
-            # If number of elements is not a power of two.
+                    indexPairListLeft = indexPairList[:int(len(indexPairList)/2)]
+                    indexPairListRight = indexPairList[int(len(indexPairList)/2):]
+                    leftElement = self.retrieve(indexPairListLeft)
+                    rightElement =  self.retrieve(indexPairListRight)
+                elemContainer.append(leftElement)
+                elemContainer.append(rightElement)
+            # Case 1: If number of matrix elements referenced in indexPairList is *not* power of two.
+            # e.g M_i,j M_j,k M_k,l M_l,m M_m,n (5 matrix elements = 2^2+2^0)
             else: 
-                # Bitscan over length of indexPairList
+                # Split referenced matrix elements into batches of 2-power sizes.
                 indexPairListCopy = indexPairList
                 for i in range(len(binList)):
                     if int(binStr[binListLen-1-i]):
-                        # For active bit a non-zero index: 
                         if i == 0:
                             # Append single element to container.
                             elementIdxPair = indexPairListCopy[-1]
                             indexPairListCopy = indexPairListCopy[:-1]
                             elemContainer.append(self.matrix[elementIdxPair[0]][elementIdxPair[1]])
                         else: 
-                            # Should break down 
+                            # Append batch of elements (of the 2-power size) to container.
                             elementIdxPairs = indexPairListCopy[-2**i:]
-                            print(i, elementIdxPairs)
                             indexPairListCopy = indexPairListCopy[:-2**i]
                             elemContainer.append(self.retrieve(elementIdxPairs)) 
-                # Multiply elements in elemContainer
-                res = reduce(lambda x, y: x * y, elemContainer)
-                # Add to hash table.
-                newKey = hash(tuple(sorted(indexPairList)))
-                self.table[newKey] = res
-                return res
+            # Multiply elements in elemContainer
+            res = reduce(lambda x, y: x * y, elemContainer)
+            # Add to hash table.
+            newKey = hash(tuple(sorted(indexPairList)))
+            self.table[newKey] = res
+            return res
             
 Test = MultLookup([[1,2,3],
                    [4,5,6],
                    [7,8,9]])
 
-print(Test.retrieve([(1,1),(2,2),(1,2),(2,1)]))
+# 5, 9, 6, 8
+print(Test.retrieve([(1,1),(2,2),(1,2),(2,1),(0,0)]))
 
 
 
