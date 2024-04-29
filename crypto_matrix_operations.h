3a9c466302e0112d3b46b33544759a7b2239373c
James Chiang <james.chiangwu@gmail.com>
2024-04-29 20:50:39 +0200

    Remove comments, dead code.

--- crypto_matrix_operations.h
+++ crypto_matrix_operations.h
@@ -10,6 +10,11 @@
 
 using namespace lbcrypto;
 
+Ciphertext<DCRTPoly> evalDiagMatrixVecMult(std::vector<Ciphertext<DCRTPoly>> &encMatDiagonals, // Must be filled.
+                                           Ciphertext<DCRTPoly> encVec,                        // Must be filled.
+                                           CryptoContext<DCRTPoly> &cryptoContext); 
+                
+
 // Class initializes rotation keys and encrypted masks.
 class InitMatrixMult {
 public:
@@ -28,61 +33,11 @@ private:
     Ciphertext<DCRTPoly> _matrixMask;
 };
 
+
 Ciphertext<DCRTPoly> evalMatrixMult(CryptoContext<DCRTPoly> &cryptoContext, 
                                     Ciphertext<DCRTPoly> encA,
                                     Ciphertext<DCRTPoly> encB,
                                     InitMatrixMult &initMatrixMult);
-
-Ciphertext<DCRTPoly> evalDiagMatrixVecMult(std::vector<Ciphertext<DCRTPoly>> &encMatDiagonals, // Must be filled.
-                                           Ciphertext<DCRTPoly> encVec,                        // Must be filled.
-                                           CryptoContext<DCRTPoly> &cryptoContext); 
-
-// std::vector<std::vector<Ciphertext<DCRTPoly>>> // Element-wise-encrypted output matrix.
-//     evalMatrixMul2Pow(std::vector<std::vector<std::vector<Ciphertext<DCRTPoly>>>> &encMatsElems, // Elem-wise encrypted input matrices.
-//                       int packingMode, // 0: row/col | 1: full matrix
-//                       CryptoContext<DCRTPoly> &cryptoContext,
-//                       InitRotsMasks &initRotsMasks,
-//                       CryptoOpsLogger &cryptoOpsLogger);
-
-// std::vector<std::vector<std::vector<Ciphertext<DCRTPoly>>>> // Element-wise-encrypted output matrix.
-//     evalMatSquarings(std::vector<std::vector<Ciphertext<DCRTPoly>>> &encMatElems, // Row-wise-encrypted input matrix.
-//                      int sqs,
-//                      int packingMode, // 0: row/col | 1: full matrix
-//                      CryptoContext<DCRTPoly> &cryptoContext,
-//                      InitRotsMasks &initRotsMasks,
-//                      CryptoOpsLogger &cryptoOpsLogger,
-//                      KeyPair<DCRTPoly> &keyPair);
-
-// // Fast, but incurs log(n) additional mul depth. 
-// std::vector<std::vector<Ciphertext<DCRTPoly>>> 
-//         evalMatSqMul(std::vector<std::vector<Ciphertext<DCRTPoly>>> &encMatElems, 
-//                      int exponent,
-//                      int packingMode, // 0: row/col | 1: full matrix
-//                      CryptoContext<DCRTPoly> &cryptoContext,
-//                      InitRotsMasks &initRotsMasks,
-//                      CryptoOpsLogger &cryptoOpsLogger,
-//                      KeyPair<DCRTPoly> &keyPair);
-
-// // Very slow but optimal mul depth. For example, M^4(r=row, c=col) = sum_{i,j,k} M_ri x M_ij x M_jk x M_kc
-// std::vector<Ciphertext<DCRTPoly>> evalMatrixExp(std::vector<Ciphertext<DCRTPoly>> &encRows, 
-//                                                 int exponent,
-//                                                 CryptoContext<DCRTPoly> &cryptoContext,
-//                                                 InitRotsMasks &initRotsMasks,
-//                                                 CryptoOpsLogger &CryptoOpsLogger); 
-
-// Ciphertext<DCRTPoly> evalMatrixVecMult(std::vector<Ciphertext<DCRTPoly>> &encRows, 
-//                                        Ciphertext<DCRTPoly> &enc_vec,
-//                                        CryptoContext<DCRTPoly> &cryptoContext,            
-//                                        InitRotsMasks &initRotsMasks);
-
-
-// Ciphertext<DCRTPoly> evalVecMatrixMult(Ciphertext<DCRTPoly> &enc_vec,
-//                                        std::vector<Ciphertext<DCRTPoly>> &encRows, 
-//                                        CryptoContext<DCRTPoly> &cryptoContext,            
-//                                        InitRotsMasks &initRotsMasks,
-//                                        CryptoOpsLogger &cryptoOpsLogger);
-
-
 
 
 #endif
